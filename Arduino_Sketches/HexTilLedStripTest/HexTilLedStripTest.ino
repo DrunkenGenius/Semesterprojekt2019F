984e9c257d3a8cfd48998c8fa98f75e9b8af9b52
Unknown <jakob.most@hotmail.com>
2019-04-11 13:04:12 +0200

    Kan sender farver i array til ledstrip

---
+++ Arduino_Sketches/HexTilLedStripTest/HexTilLedStripTest.ino
@@ -0,0 +1,48 @@
+#include <Adafruit_DotStar.h>
+#include <SPI.h>
+
+#define NUMPIXELS 60 // Number of LEDs in strip
+
+// Here's how to control the LEDs from any two pins:
+#define DATAPIN    4
+#define CLOCKPIN   5
+
+// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
+Adafruit_DotStar strip(NUMPIXELS, DOTSTAR_BRG);
+
+
+//int colorArray[21][3] = {{0,255,0},{0,255,50},{0,255,100},{0,255,150},{0,255,255},{0,100,255}k,{0,0,255},{50,0,255},{150,0,255},{255,0,255},{255,0,200},{255,0,100},{255,0,50},{255,0,0},{255,40,0},{255,75,0},{255,100,0},{255,150,0},{255,200,0},{255,255,0},{150,255,0}};
+uint32_t colorHexArray[21] = {0xFF0000,0xFF0032,0xFF0064,0xFF0096,0xFF00FF,0x6400FF,0x0000FF,0x0032FF,0x9600FF,0x00FFFF,0x00FFC8,0x00FF64,0x00FF32,0x00FF00,0x28FF00,0x4BFF00,0x64FF00,0x96FF00,0xC8FF00,0xFFFF00,0x96FF00};
+
+//HUSK DET ER GRB
+
+
+void setup() {
+  
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+  clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+#endif
+
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
+
+}
+//________________________________________________________________
+
+
+void loop() {
+  
+
+  for (int i = 0 ; i < 22 ; i++){
+    uint32_t color = colorHexArray[i];
+    setColor(color);
+    delay(1000);
+  }
+}
+
+void setColor(uint32_t color){
+  for(int i=0; i<10; i++){
+    strip.setPixelColor(i, color);
+    }
+    strip.show(); //refresh
+  }
